fcb2c65a4a306cd29194e09fb20a0ed677f7306d
Vince <45806875+vince1444@users.noreply.github.com>
2021-01-30 01:27:05 -0500

    Added Filtering to getRSSFeed()

--- app.js
+++ app.js
@@ -12,20 +12,29 @@ const tickers = {};
 
 //.map(ele => ele.toUpperCase()).filter( (x, y, z) => (x))
 function getTickers(string) {
-    if(string.includes('$')) {
-        return string.toUpperCase().split('$')
+    let str = string.toUpperCase();
+    if(str.includes('$')) {
+        return str.split('$')
         .map(t => t.split(' ')).flat()
+        .filter(x => x.trim(''))
         .filter(x => x.length < 5)
         .filter(x => x.match(/^[A-Z]/))
         .filter((element, index, array) => (array.indexOf(element) === index))
-        .filter(x => x.trim(''));
-        //.filter(x => string.includes(`$${x}`));
-        //.filter(x => x.match(/^[A-Z]/));
-        //.map(t => t.toUpperCase());
+        .filter(x => str.includes(`$${x}`))
+        .map(x => x.replace(/[^\w\s]/gi, ''));
     }
     return [];
 }
 
+function containsTickers(ticksArray, string) {
+    let present = false;
+    ticksArray.forEach(ele => {
+        if (string.toUpperCase().includes(ele)) {
+            present = true;
+        }
+    })
+    return present;
+}
 
 function getRSSFeed(url) {
     axios.get(url)
@@ -34,39 +43,50 @@ function getRSSFeed(url) {
         let parsedData = parser.parse(res.data);
         let parsedArray = parsedData.feed.entry
         //goes through each element and transforms
-        parsedArray.forEach(element => {
-            let id = element.id;
-            let author = element.author.uri;
-            let title = he.decode(element.title);
-            let content = he.decode(element.content);
-            if (!posts[id]) {
-                /*let t = [
-                    ...getTickers(title),
-                    ...getTickers(content)
-                ]*/
-                
-                let x = getTickers(title);
-                console.log(x);
-
-               /*t.forEach(tick => {
-                    console.log(tick);
-                    //tickers[tick] = tickers[tick] ? tickers[tick]++ : 1
-                })*/
-
-                let post = {
-                    //shorthand for author: author
-                    author,
-                    title,
-                    content
-                }
-
-                //dictionary key to value
-                posts[id] = post;
-            }
+        parsedArray.forEach(ele => {
+            posts[ele.id] = parsePost(ele);
+            
+            //destructuring as a param
         })
-        //console.log(posts);
+        let x = parsedArray.filter(({id}) => posts[id])
+        
+        x.forEach( ele => {
+            posts[ele.id] = parsePost(ele);
+        })
+        console.log(parsedArray.length);
+        console.log(x.length);
     })
 }
+
+
+
+function parsePost(post) {
+    let id = post.id;
+    let author = post.author.uri;
+    let title = he.decode(post.title);
+    let content = he.decode(post.content);
+    let ticks = Object.keys(tickers, title);
+    if (!posts[id]) {
+        //combines 2 arrays into 1
+        let t = [
+            ...getTickers(title),
+            ...getTickers(content)
+        ]
+    
+        t.forEach(tick => {
+            tickers[tick] = tickers[tick] ? tickers[tick] + 1 : 1;
+        })
+        
+        if (t.length || (containsTickers(ticks, title) && containsTickers(ticks, content))) {
+            return {
+                author,
+                title,
+                content
+            }
+        }
+    }
+}
+
 
 getRSSFeed('https://www.reddit.com/r/wallstreetbets/.rss');
 
