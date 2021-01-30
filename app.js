ac17ef835e2cef77eff6182ec4a302c07f724fc3
unitehenry <unitehenry@gmail.com>
2021-01-30 06:01:21 -0500

    mongodb client

--- app.js
+++ app.js
@@ -1,95 +1,103 @@
-//1-29-2021
-
-
-//imports
-const axios = require('axios').default;
+const axios = require('axios');
 const parser = require('fast-xml-parser');
 const he = require('he');
 
+const { sendTickers, getLatestBets } = require('./moongo');
+
 const posts = {};
 const tickers = {};
-//functions
 
-//.map(ele => ele.toUpperCase()).filter( (x, y, z) => (x))
 function getTickers(string) {
-    let str = string.toUpperCase();
-    if(str.includes('$')) {
-        return str.split('$')
-        .map(t => t.split(' ')).flat()
-        .filter(x => x.trim(''))
-        .filter(x => x.length < 5)
-        .filter(x => x.match(/^[A-Z]/))
-        .filter((element, index, array) => (array.indexOf(element) === index))
-        .filter(x => str.includes(`$${x}`))
-        .map(x => x.replace(/[^\w\s]/gi, ''));
-    }
-    return [];
+  let str = string.toUpperCase();
+  if (str.includes('$')) {
+    return str
+      .split('$')
+      .map((t) => t.split(' '))
+      .flat()
+      .filter((x) => x.trim(''))
+      .filter((x) => x.length < 5)
+      .filter((x) => x.match(/^[A-Z]/))
+      .filter((element, index, array) => array.indexOf(element) === index)
+      .filter((x) => str.includes(`$${x}`))
+      .map((x) => x.replace(/[^\w\s]/gi, ''));
+  }
+  return [];
 }
 
 function containsTickers(ticksArray, string) {
-    let present = false;
-    ticksArray.forEach(ele => {
-        if (string.toUpperCase().includes(ele)) {
-            present = true;
-        }
-    })
-    return present;
+  let present = false;
+  ticksArray.forEach((ele) => {
+    if (string.toUpperCase().includes(ele)) {
+      present = true;
+    }
+  });
+  return present;
 }
 
 function getRSSFeed(url) {
-    axios.get(url)
-    .then(res => {
-        //parses xml into json obj
-        let parsedData = parser.parse(res.data);
-        let parsedArray = parsedData.feed.entry
-        //goes through each element and transforms
-        parsedArray.forEach(ele => {
-            posts[ele.id] = parsePost(ele);
-            
-            //destructuring as a param
-        })
-        let x = parsedArray.filter(({id}) => posts[id])
-        
-        x.forEach( ele => {
-            posts[ele.id] = parsePost(ele);
-        })
-        console.log(parsedArray.length);
-        console.log(x.length);
-    })
+  return new Promise(resolve => {
+    axios.get(url).then((res) => {
+      
+      let parsedData = parser.parse(res.data);
+      let parsedArray = parsedData.feed.entry;
+      
+      parsedArray.forEach((ele) => {
+        posts[ele.id] = parsePost(ele);
+      });
+
+      let remaining = parsedArray.filter(({ id }) => posts[id]);
+
+      if(remaining.length) {
+        remaining.forEach((ele, idx) => {
+          posts[ele.id] = parsePost(ele);
+          (idx === remaining.length - 1) && resolve(tickers);
+        });
+      } else {
+        resolve(tickers);
+      }
+
+    });
+
+  }); 
+  
 }
-
-
 
 function parsePost(post) {
-    let id = post.id;
-    let author = post.author.uri;
-    let title = he.decode(post.title);
-    let content = he.decode(post.content);
-    let ticks = Object.keys(tickers, title);
-    if (!posts[id]) {
-        //combines 2 arrays into 1
-        let t = [
-            ...getTickers(title),
-            ...getTickers(content)
-        ]
-    
-        t.forEach(tick => {
-            tickers[tick] = tickers[tick] ? tickers[tick] + 1 : 1;
-        })
-        
-        if (t.length || (containsTickers(ticks, title) && containsTickers(ticks, content))) {
-            return {
-                author,
-                title,
-                content
-            }
-        }
+  let id = post.id;
+  let author = post.author.uri;
+  let title = he.decode(post.title);
+  let content = he.decode(post.content);
+  let ticks = Object.keys(tickers, title);
+  if (!posts[id]) {
+    let t = [...getTickers(title), ...getTickers(content)];
+
+    t.forEach((tick) => {
+      tickers[tick] = tickers[tick] ? tickers[tick] + 1 : 1;
+    });
+
+    if (
+      t.length === 0 &&
+      !(containsTickers(ticks, title) && containsTickers(ticks, content))
+    ) {
+      return {
+        author,
+        title,
+        content,
+      };
     }
+  }
 }
 
+async function main() {
+  console.log('[BEST]:', await getRSSFeed('https://www.reddit.com/r/wallstreetbets/best/.rss'));
+  console.log('[HOT]:', await getRSSFeed('https://www.reddit.com/r/wallstreetbets/hot/.rss'));
+  console.log('[NEW]:', await getRSSFeed('https://www.reddit.com/r/wallstreetbets/new/.rss'));
+  console.log('[TOP]:', await getRSSFeed('https://www.reddit.com/r/wallstreetbets/top/.rss'));
+  console.log('[RISING]:', await getRSSFeed('https://www.reddit.com/r/wallstreetbets/rising/.rss'));
 
-getRSSFeed('https://www.reddit.com/r/wallstreetbets/.rss');
+  console.log('[FINAL TICKER COUNT]:', tickers);
 
+  await sendTickers(tickers);
+}
 
-
-
+main();
